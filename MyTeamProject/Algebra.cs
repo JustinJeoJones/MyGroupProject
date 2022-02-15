474ef35b6e81c5a20c4599f3d2d3b104c2556f81
Jeo <jeo961@gmail.com>
2022-02-15 13:33:31 -0500

    Added AlgeBruh

---
+++ MyTeamProject/Algebra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTeamProject
+{
+    internal class Algebra
+    {
+        public int Adder(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subber(int x, int y)
+        {
+            return x + y;
+        }
+        public int Multi(int x, int y)
+        {
+            return x * y;
+        }
+        public int Divi(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
